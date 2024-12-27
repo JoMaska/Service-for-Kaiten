677708373a75d5df7a8e01c205e8d758dddac54b
Maska <jojomas900@gmail.com>
2024-12-27 19:46:04 +0300

    fix bugs

--- request.py
+++ request.py
@@ -3,5 +3,4 @@ import aiohttp
 async def make_request(url, method, data, headers):
     async with aiohttp.ClientSession() as session:
         async with session.request(url=url, method=method, json=data, headers=headers) as response:
-            print('\nMake request : \n',url, method, data, headers)
             return await response.json()
